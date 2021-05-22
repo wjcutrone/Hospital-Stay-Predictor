2bee99f9fc7ac017713e86d0248f62b8a0dae408
Mona Peteet <mpeteet@comcast.net>
2021-05-22 10:45:40 -0400

    Postgres sql

---
+++ QuickDBD-ML_test_data.sql
@@ -0,0 +1,24 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "test_data" (
+    "case_id" int,
+    "Hospital_code" int,
+    "Hosptial_type_code" varchar(50),
+    "City_Code_Hospital" int,
+    "Hospital_region_code" varchar(50),
+    "Available Extra Rooms in Hospital" int,
+    "Department" varchar(100),
+    "Ward_Type" varchar(50),
+    "Ward_Facility_Code" varchar(50),
+    "Bed Grade" float,
+    "patientid" int,
+    "City_Code_Patient" float,
+    "Type of Admission" varchar(100),
+    "Severity of Illness" varchar(100) ,
+    "Visitors with Patient" float,
+    "Age" varchar(50) ,
+    "Admission_Deposit" float
+);
+
