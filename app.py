fdf9b06d76a8bea604e59bc8ccd5787e5b4e532c
Stephen Domke <stephencdomke@gmail.com>
2021-05-28 16:14:26 -0400

    adding changes

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ from flask import Flask, jsonify, render_template, request, g, redirect
 from tensorflow.keras.models import load_model
 import pandas as pd
 import psycopg2
-import sqlite3
 import os
-# from model_files.ml_model import the-name-of-the-model
 # sklearn libraries need to be imported
 # from sklearn.preprocessing import StandardScaler
 
@@ -18,60 +16,76 @@ import os
 #########################################
 app = Flask(__name__)
 
-#########################################
-# Database setup
-#########################################
-# Create engine and connection
-# engine = create_engine(f"postgresql://hospital-stay-postgresdb.czsgahc5qz1z.us-east-2.rds.amazonaws.com")
-# conn = engine.connect()
-
-# the model needs to be loaded and possibly the prepossesser or scaler
-model = load_model(modelpath and saved-model-name)
-# model = joblib.load(saved-model-name.pkl')
-# sc = load(modelpath and saved-scaler-name)
-
-# Create a test route and function that returns a string
-@app.route("/test", methods=['GET'])
-def test():
-    return "Pinging model appplication!"
-
-
 # Create the route that renders the index.html template
- @app.route("/")
+@app.route("/")
 def home():
     return render_template("index.html")
 
 # Create a route to get the user inputs and send them to the model
-@app.route("/predict", methods=['GET','POST'])
-def predict();
+@app.route("/predict", methods=['GET', 'POST'])
+def predict(input):
     if request.method == 'POST':
-        hospcode = request.form["Hospital_code"]
-        hosptype = request.form["Hospital_type_code"]
-        #hospcity = request.form["City_Code_Hospital"]
-        hospregion = request.form["Hospital_region_code"]
-        extrarooms= request.form["Available Extra Rooms in Hospital"]
-        dept = request.form["Department"]
-        ward = request.form["Ward_Type"]
-        wardcode = request.form["Ward_Facilty_Code"]
-        bedgrade = request.form["Bed Grade"]
-        #patientcity = request.form["City_Code_Patient"]
-        admtype = request.form["Type of Admission"]
-        severity = request.form["Severity of Illness"]
-        visitors = request.form["Visitors with Patient"]
-        age = request.form["Age"]
-        deposit = request.form["Admission_Deposit"]
+        Admission_Deposit = request.form["Admission_Deposit"]
+        Age = request.form["Age"]
+        Available_Extra_Rooms_in_Hospital = request.form["Available Extra Rooms in Hospital"]
+        Bed_Grade = request.form["Bed Grade"]
+        City_Code_Hospital = request.form["City_Code_Hospital"]
+        City_Code_Patient = request.form["City_Code_Patient"]
+        Department = request.form["Department"]
+        Hospital_code = request.form["Hospital_code"]
+        Hospital_region_code = request.form["Hospital_region_code"]
+        Hospital_type_code = request.form["Hospital_type_code"]
+        Severity_of_Illness = request.form["Severity of Illness"]
+        Type_of_Admission = request.form["Type of Admission"]
+        Visitors_with_Patient = request.form["Visitors with Patient"]
+        Ward_Facility_Code = request.form["Ward_Facilty_Code"]
+        Ward_Type = request.form["Ward_Type"]
 
-# Create a dataframe that holds the user inputs and if needed run the preprocessor
-    row_df = pd.DataFrame([pd.Series([hospcode, hosptype, hospregion, extrarooms, dept, ward, wardcode, bedgrade, admtype, severity, visitors, age, deposit])])
-    row_df = pd.DataFrame(std.transform(row_df))
+        input = {"Admission_Deposit": Admission_Deposit,
+                 "Age": Age,
+                 "Available Extra Rooms in Hospital": Available_Extra_Rooms_in_Hospital,
+                 "Bed Grade": Bed_Grade,
+                 "City_Code_Hospital": City_Code_Hospital,
+                 "City_Code_Patient": City_Code_Patient,
+                 "Department": Department,
+                 "Hospital_code": Hospital_code,
+                 "Hospital_region_code": Hospital_region_code,
+                 "Hospital_type_code": Hospital_type_code,
+                 "Severity of Illness": Severity_of_Illness,
+                 "Type of Admission": Type_of_Admission,
+                 "Visitors with Patient": Visitors_with_Patient,
+                 "Ward_Facility_Code": Ward_Facilty_Code,
+                 "Ward_Type": Ward_Type}
+        
 
-# Predict the probability and return the result
-    prediction = model.predict(row_df)
-    output=(prediction)
+    translators = None
+    with open('translators.json', 'r') as f:
+        translators=json.load(f)
 
-    return (result)
+    X_translator = translators['X_translator']
+    scale_translator = translators['scale_translator']
+    order = translators['data_order']
+    y_translator = translators['y_translator']
+    input_t = {}
+    for (category, value) in input.items():
+        if category in x_categorical_columns:
+            input_t[category] = X_translator[category][str(value)]
+        elif category in x_numerical_columns:
+            mean = scale_translator[category]['mean']
+            std = scale_translator[category]['standard_deviation']
+            value = (value - mean)/std
+            input_t[category] = value
+        else:
+            print(f'ERROR: Unsupported parameter found! {category}') 
 
+    input_t = np.array([input_t[i] for i in order]).reshape(1, 15)
+    model_1 = create_model()
+    model_1.load_weights('training_1/cp-0047.ckpt')
+    prediction = model_1.predict(input_t).argmax()
+    prediction = y_translator[str(prediction)]
+    return render_template("predict.html", prediction=prediction)
 
+predict(input)
 
 
 if __name__ == "__main__":
