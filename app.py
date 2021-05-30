ffdc60e17d08dac586b3b12e6fd51f22ffcd88a3
Stephen Domke <stephencdomke@gmail.com>
2021-05-30 17:44:41 -0400

    adding files

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 # Import necessary libraries
-import sqlalchemy
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, inspect
 from flask import Flask, jsonify, render_template, request, g, redirect
 from tensorflow.keras.models import load_model
 import pandas as pd
 import psycopg2
 import os
 import json
-# sklearn libraries need to be imported
-# from sklearn.preprocessing import StandardScaler
+import numpy as np
+import h5py
 
+x_categorical_columns = ['Hospital_type_code',
+                            'Hospital_region_code',
+                            'Department',
+                            'Ward_Type',
+                            'Ward_Facility_Code',
+                            'Type of Admission',
+                            'Severity of Illness',
+                            'Age',
+                            'Bed Grade',
+                            'Hospital_code',
+                            'City_Code_Hospital',
+                            'City_Code_Patient']
+x_numerical_columns = ['Available Extra Rooms in Hospital',
+                            'Visitors with Patient',
+                            'Admission_Deposit']
+translators = None
 
 #########################################
 # Flask Setup
@@ -26,9 +39,9 @@ def home():
 @app.route("/", methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
-        Admission_Deposit = request.form["Admission_Deposit"]
+        Admission_Deposit = float(request.form["Admission_Deposit"])
         Age = request.form["Age"]
-        Available_Extra_Rooms_in_Hospital = request.form["Available Extra Rooms in Hospital"]
+        Available_Extra_Rooms_in_Hospital = float(request.form["Available Extra Rooms in Hospital"])
         Bed_Grade = request.form["Bed Grade"]
         City_Code_Hospital = request.form["City_Code_Hospital"]
         City_Code_Patient = request.form["City_Code_Patient"]
@@ -38,7 +51,7 @@ def predict():
         Hospital_type_code = request.form["Hospital_type_code"]
         Severity_of_Illness = request.form["Severity of Illness"]
         Type_of_Admission = request.form["Type of Admission"]
-        Visitors_with_Patient = request.form["Visitors with Patient"]
+        Visitors_with_Patient = float(request.form["Visitors with Patient"])
         Ward_Facility_Code = request.form["Ward_Facility_Code"]
         Ward_Type = request.form["Ward_Type"]
 
@@ -58,8 +71,7 @@ def predict():
                  "Ward_Facility_Code": Ward_Facility_Code,
                  "Ward_Type": Ward_Type}
         
-
-    translators = None
+    print(input)
     with open('translators.json', 'r') as f:
         translators=json.load(f)
 
@@ -69,8 +81,12 @@ def predict():
     y_translator = translators['y_translator']
     input_t = {}
     for (category, value) in input.items():
+        print(category, value)
         if category in x_categorical_columns:
-            input_t[category] = X_translator[category][str(value)]
+            try:
+               input_t[category] = X_translator[category][str(value)]
+            except:
+                pass
         elif category in x_numerical_columns:
             mean = scale_translator[category]['mean']
             std = scale_translator[category]['standard_deviation']
@@ -80,12 +96,11 @@ def predict():
             print(f'ERROR: Unsupported parameter found! {category}') 
 
     input_t = np.array([input_t[i] for i in order]).reshape(1, 15)
-    model_1 = create_model()
-    model_1.load_weights('training_1/cp-0047.ckpt')
+    model_1 = load_model('model_1.h5', compile=False)
     prediction = model_1.predict(input_t).argmax()
     prediction = y_translator[str(prediction)]
     return render_template("index.html", prediction=prediction)
 
 
 if __name__ == "__main__":
-    app.run() 
+    app.run()
